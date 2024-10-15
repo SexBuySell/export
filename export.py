04c93d34cb957e01bdb3bbf08c54559f46c13f0f
SexBuySell <sexbuysell@gmail.com>
2024-10-15 17:34:54 +0300

    Update export.py

--- export.py
+++ export.py
@@ -1,18 +1,14 @@
 import urllib.request
 import lxml.etree as ET
 import logging
-import os
 
-# Отримуємо поточну робочу директорію з $GITHUB_WORKSPACE
-workspace = os.getenv('GITHUB_WORKSPACE', '.')
 
 # Налаштування логування без мілісекунд, вказуємо шлях до логу
-log_file_path = os.path.join(workspace, 'script_log.log')
 logging.basicConfig(
-    filename=log_file_path,
+    filename='script_log.log',  # Заміни цей шлях на потрібний
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
+    datefmt='%Y-%m-%d %H:%M:%S'  # Формат без мілісекунд
 )
 
 # URL прайс-листу
@@ -24,8 +20,6 @@ try:
 
     # Перевірка на успішне завантаження
     if response.status == 200:
-        logging.info('XML файл успішно завантажено.')
-        
         # Розбір XML
         content = response.read()
         root = ET.fromstring(content)
@@ -33,26 +27,18 @@ try:
         # Список категорій, які потрібно видалити
         categories_to_delete = ['6533', '6534', '6535', '4848', '4917', '2621', '4799', '4801', '10', '5467', '4860', '4898', '4899', '4900', '4901', '7029', '9698', '4866', '4870', '4882', '4883', '4893', '4894', '4906', '4902', '4903', '4904', '4905', '5461', '8203', '5302', '4794', '4797', '8181', '15080', '17831', '9495', '9496', '4878', '4880', '2316']
 
-        deleted_offers_count = 0  # Лічильник видалених елементів
         # Пройдемося по кожному <offer> елементу і видалимо його, якщо категорія в списку categories_to_delete
         for offer in root.xpath('//offer'):
             category_element = offer.find('.//categoryId')
             if category_element is not None and category_element.text in categories_to_delete:
                 offer.getparent().remove(offer)
-                deleted_offers_count += 1
-
-        if deleted_offers_count > 0:
-            logging.info(f'Видалено {deleted_offers_count} елементів.')
-        else:
-            logging.info('Не було видалено жодного елемента.')
 
         # Запишемо оновлений XML у файл
-        output_file_path = os.path.join(workspace, 'import.xml')
-        with open(output_file_path, 'wb') as new_price_list:
+        with open('import.xml', 'wb') as new_price_list:
             new_price_list.write('<?xml version="1.0" encoding="UTF-8"?>\n'.encode('utf-8'))
             new_price_list.write(ET.tostring(root, encoding='utf-8'))
 
-        logging.info('Скрипт виконано успішно, файл import.xml оновлено.')
+        logging.info('Скрипт виконано успішно')
     else:
         logging.error(f'Помилка при завантаженні: {response.status}')
 except Exception as e:
